c783a5dcf38b6122a7e0c7544dc5704018dd8af4
UG-Andi <hirzberger.ah@hotmail.com>
2016-01-29 15:08:34 +0100

    simon

---
+++ Build It Up/Assets/Scripts/BuildingPlacement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPlacement : MonoBehaviour {
+
+    private Transform currentBuilding;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (currentBuilding != null)
+        {
+            Vector3 m = Input.mousePosition;
+            m = new Vector3(m.x, m.y, transform.position.y);
+            Vector3 p = GetComponent<Camera>().ScreenToWorldPoint(m);
+            currentBuilding.position = new Vector3(p.x, 0, p.z);
+        }
+	}
+
+    public void SetItem(GameObject b)
+    {
+        currentBuilding = ((GameObject)Instantiate(b)).transform;
+    }
+}
