b8ad132d201f9d8dc855c52f2e6ab868bca24f2d
UG-Andi <hirzberger.ah@hotmail.com>
2016-01-29 15:47:49 +0100

    nearly full buildingplacement

--- Build It Up/Assets/Scripts/BuildingPlacement.cs
+++ Build It Up/Assets/Scripts/BuildingPlacement.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class BuildingPlacement : MonoBehaviour {
 
+    private PlaceableBuildings placeableBuilding;
     private Transform currentBuilding;
+    private bool hasPlaced;
+
+    public LayerMask buildingsMask;
 
 	// Use this for initialization
 	void Start ()
@@ -14,17 +18,49 @@ public class BuildingPlacement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	    if (currentBuilding != null)
+        Vector3 m = Input.mousePosition;
+        m = new Vector3(m.x, m.y, transform.position.y);
+        Vector3 p = GetComponent<Camera>().ScreenToWorldPoint(m);
+
+        if (currentBuilding != null && !hasPlaced)
         {
-            Vector3 m = Input.mousePosition;
-            m = new Vector3(m.x, m.y, transform.position.y);
-            Vector3 p = GetComponent<Camera>().ScreenToWorldPoint(m);
             currentBuilding.position = new Vector3(p.x, 0, p.z);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (IsLegalPosition())
+                {
+                hasPlaced = true;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit = new RaycastHit();
+                Ray ray = new Ray(new Vector3(p.x, 500, p.z), Vector3.down);
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, buildingsMask))
+                {
+                    Debug.Log(hit.collider.name);
+                }
+            }
         }
 	}
 
+    bool IsLegalPosition()
+    {
+        if (placeableBuilding.colliders.Count > 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
     public void SetItem(GameObject b)
     {
+        hasPlaced = false;
         currentBuilding = ((GameObject)Instantiate(b)).transform;
+        placeableBuilding = currentBuilding.GetComponent<PlaceableBuildings>();
     }
 }
--- Build It Up/Assets/Scripts/PlaceableBuildings.cs
+++ Build It Up/Assets/Scripts/PlaceableBuildings.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class PNewBehaviourScript : MonoBehaviour {
+public class PlaceableBuildings : MonoBehaviour {
+
+    [HideInInspector]
+    public List<Collider> colliders = new List<Collider>();
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Building")
+        {
+            colliders.Add(c);
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Building")
+        {
+            colliders.Remove(c);
+        }
+    }
 }
